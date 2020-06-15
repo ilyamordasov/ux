86b54b55255f081db8c8d94c4aa7fa24d8529609
ilyamordasov <carlogulliani@gmail.com>
2020-06-15 15:17:00 +0300

    Refactoring

--- prototypes/chat/src/App.js
+++ prototypes/chat/src/App.js
@@ -1,107 +1,99 @@
-import React,{ useState, useEffect } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {Widget, renderCustomComponent, addResponseMessage} from 'react-chat-widget';
+import {Widget, addResponseMessage} from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
 
 import {DropdownButton,Dropdown} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import zulip from 'zulip-js';
+
 class App extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {lastMessage: 9999999999999999, channel:"", channels:[]}
-    this.getAllChannels();
+    this.state = {
+      queue_id: -99999,
+      last_event_id: 0,
+      channel:"",
+      channels:[],
+    }
+
+    this.intervalID = -1;
+
+    this.config = {
+      username: process.env.REACT_APP_ZULIP_UNAME,
+      apiKey: process.env.REACT_APP_ZULIP_KEY,
+      realm: process.env.REACT_APP_ZULIP_URL
+    };
   }
 
   componentDidMount() {
+    this.getAllChannels();
     addResponseMessage("Hey there! Hope you're staying safe and healthy. Can I ask you a question?");
-    this.intervalID = setInterval(this.fetchMessages, 1000);
+    zulip(this.config).then(client => {
+      if (this.state.queue_id === -99999) {
+        client.queues.register().then(res => {
+          this.setState({queue_id : res.queue_id});
+          this.setState({last_event_id : res.last_event_id});
+        });
+      }
+
+      this.intervalID = setInterval(() => {
+        client.events.retrieve({queue_id:this.state.queue_id,last_event_id:this.state.last_event_id}).then(res => {
+          for (const event in res.events) {
+            this.setState({last_event_id : Math.max(this.state.last_event_id, parseInt(res.id))});
+            if (event.hasOwnProperty("message")) {
+              addResponseMessage(event.message.content);
+            }
+          }
+        });
+      }, 1000);
+    });
   }
+
+  handleNewUserMessage = (message) => {
+    zulip(this.config).then(async (client) => {
+      var requestOptions = {
+        to: this.state.channel,
+        type: 'stream',
+        subject: '+89030000000',
+        content: message
+      };
+      return await client.messages.send(requestOptions);
+    });
+  }
+
+  setTypingStatus = (to, flag) => {
+    zulip(this.config).then(client => {
+      client.typing.send();
+    });
+  };
+
+  getAllChannels = () => {
+    zulip(this.config).then(client => {
+      client.streams.retrieve().then(res => {
+        var streams = [];
+        for (const stream of res.streams) {
+          streams.push({name:stream.name, invite_only:stream.invite_only});
+        }
+        this.setState({channels:streams, channel:streams[0].name});
+      });
+    });
+  }
+
+  chooseChannel = (e) => {
+    console.log(e);
+    this.setState({channel:e});
+  };
 
   componentWillUnmount() {
     clearInterval(this.intervalID);
   }
 
-  fetchMessages = () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Basic " + process.env.REACT_APP_ZULIP_TOKEN);
-
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-    fetch(process.env.REACT_APP_ZULIP_URL + "/messages?anchor=" + this.state.lastMessage + "&num_before=0&num_after=10&narrow=[{\"operator\": \"stream\", \"operand\": \"test\"}]", requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        result.messages.map(item => {
-          if (item.subject === "+79030000000" && item.id !== this.state.lastMessage) {
-            addResponseMessage(item.content.replace(/<\/?[^>]+(>|$)/g, ""));
-            this.setState({lastMessage: item.id});
-          }
-        });
-      })
-      .catch(e => console.log(e));
-  }
-
-  handleNewUserMessage = (message) => {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Basic " + process.env.REACT_APP_ZULIP_TOKEN);
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("type", "stream");
-    urlencoded.append("to", this.state.channel);
-    urlencoded.append("content", message);
-    urlencoded.append("subject", "+79030000000");
-
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: 'follow'
-    };
-
-    fetch(process.env.REACT_APP_ZULIP_URL + "/messages", requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        this.setState({lastMessage: result.id});
-      })
-      .catch(error => console.log('error', error))
-  }
-
-  getAllChannels = () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Basic " + process.env.REACT_APP_ZULIP_TOKEN);
-
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-
-    fetch(process.env.REACT_APP_ZULIP_URL + "/streams?include_owner_subscribed=true", requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        console.log(result);
-        var arr = [];
-        result.streams.map(channel => {
-          arr.push({name:channel.name, invite_only:channel.invite_only});
-          if (!channel.invite_only) {
-            this.setState({channel:channel.name});
-          }
-        });
-        this.setState({channels:arr});
-      })
-      .catch(error => console.log('error', error));
-  }
-
-  chooseChannel = (e) => {
-    this.setState({channel:e});
-  };
+  
 
   render() {
     return (
@@ -110,7 +102,7 @@ class App extends React.Component {
         <DropdownButton title={this.state.channel} onSelect={this.chooseChannel}>
         {
           this.state.channels.map( item => (
-            <Dropdown.Item eventKey={item.name}>{item.name} ({item.invite_only?"private":"public"})</Dropdown.Item>
+            <Dropdown.Item eventKey={item.name} key={item.name}>{item.name} ({item.invite_only?"private":"public"})</Dropdown.Item>
           ))
         }
         </DropdownButton>
